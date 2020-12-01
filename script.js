a44dc36ffa43380d8bb14a15693f5efa72c2ccac
Alexis <alexysmcf@gmail.com>
2020-12-01 17:03:55 -0300

    Add @mediaQuerys

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ $(Document).ready(function(){
         }
     })
 })
+
+function checkInputs(inputs) {
+    var filled = true;
+
+    inputs.forEach(function(input) {
+
+        if(input.value === "") {
+            filled = false;
+        }
+
+    });
+    return filled;  
+}
+var inputs = document.querySelectorAll("input");
+var submit = document.querySelector("#submit");
+inputs.forEach(function(input) {
+
+    input.addEventListener("keyup", function() {
+        if(checkInputs(inputs)) {
+            submit.disabled = false;
+        } else {
+            submit.disabled = true;
+        }
+    });
+});
+
+function confirmation() {
+    alert("SEU CADASTRO FOI ENVIADO COM SUCESSO! ENTRAREMOS EM CONTATO COM VOCÊ O MAIS RAPIDO POSSÍVEL.");
+}
